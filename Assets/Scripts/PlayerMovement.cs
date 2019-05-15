5d7bbb22f032450505fc580728ca79ab97f3bde7
Eashvar Balachandran <eashvar.balachandran@uoit.net>
2019-05-14 20:26:23 -0400

    Movement & Collision Working

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float speed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    //get input from player
+    void Update()
+    {
+       horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //move our character
+        //ensure we move the same amount no matter how many times function is called 
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
