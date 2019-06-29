aef0ee88c81db775d9a790f55ebff7abb4a6254c
Adam Bilyea <adam.bilyea@uoit.net>
2019-06-28 20:43:49 -0400

    Akawat has joined the server

---
+++ Assets/Scripts/CraneMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraneMovement : MonoBehaviour
+{
+    private PressE myScript;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PressE.activated == true)
+        {
+            Debug.Log("Crane Is Moving");
+            rb.AddForce(transform.up * 1560);
+        }
+    }
+}
---
+++ Assets/Scripts/PressE.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressE : MonoBehaviour
+{
+    private bool inTrigger;
+    public static bool activated = false;
+    public GameObject deathEffect;
+    GameObject clone;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inTrigger = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inTrigger = false;
+        }
+    }
+
+    void Update()
+    {
+        if (inTrigger && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Activated");
+            activated = true;
+        }
+    }
+
+
+
+
+}
